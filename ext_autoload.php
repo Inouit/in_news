6c1558c858e30fef1db2b9d8b2cf791b69ddb75b
Grégory Copin <gcopin@inouit.com>
2014-03-19 17:21:58 +0100

    [upd] slice list template into partials

---
+++ ext_autoload.php
@@ -0,0 +1,5 @@
+<?php
+  $extensionPath = t3lib_extMgm::extPath('in_news');
+  return array(
+    'tx_innews_controller_newscontroller' => $extensionPath . 'Classes/Domain/Model/NewsController.php',
+  );
