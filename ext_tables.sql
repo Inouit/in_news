112a8c9ee144b94359790152f88d83f7eda08248
jviste <jviste@inouit.com>
2014-07-29 18:15:47 +0200

    [udp] - indentation

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,9 +9,9 @@ CREATE TABLE sys_category (
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
-  tx_innews_news_top tinyint DEFAULT '0' NOT NULL,
+	tx_innews_news_top tinyint DEFAULT '0' NOT NULL,
 	tx_innews_news_teaser text,
-  tx_innews_news_display_date int(11) DEFAULT '0' NOT NULL,
+	tx_innews_news_display_date int(11) DEFAULT '0' NOT NULL,
 
 	tx_innews_event_from int(11) DEFAULT '0' NOT NULL,
 	tx_innews_event_to int(11) DEFAULT '0' NOT NULL,
